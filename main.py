ed47d6cdf41b705d6d87b4e9ac64f07431e6cfc1
apu2838 <apu2838@gmail.com>
2021-05-02 22:59:52 -0400

    update on 03-May-2021 0859 hours

--- main.py
+++ main.py
@@ -861,8 +861,32 @@ dollar_to_pound = 0.76
 new_price = {item: value*dollar_to_pound for (item, value) in old_price.items()}
 print(new_price)
 """
+"""
 # Example-4: If Conditional Dictionary Comprehension
 original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
 print(original_dict)
 even_dict = {k: v for (k, v) in original_dict.items() if v % 2 == 0}
 print(even_dict)
+"""
+"""
+# Example: Multiple if Conditional Dictionary Comprehension
+original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
+print(original_dict)
+new_dict = {k: v for (k, v) in original_dict.items() if v % 2 !=0 if v < 40}
+print(new_dict)
+"""
+"""
+# Example 6: if-else Conditional Dictionary Comprehension
+original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
+print(original_dict)
+new_dict_1 = {k: ('old' if v > 40 else 'young')
+	for (k, v) in original_dict.items()}
+print(new_dict_1)
+"""
+# Example 7: Nested Dictionary with Two Dictionary Comprehensions
+dictionary = {
+	k1: {k2: k1 *k2 for k2 in range(1,6)} for k1 in range(2, 5)
+}
+print(dictionary)
+
+
